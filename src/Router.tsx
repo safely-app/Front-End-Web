205bb78afc793f22b5be78e400e1cb8fbbf35566
Louis Christner <louis.christner@epitech.eu>
2021-05-01 21:14:11 +0200

    Add style to extend AUthentication pages white box

--- src/Router.tsx
+++ src/Router.tsx
@@ -15,7 +15,15 @@ const Router: React.FC<IRouterProps> = ({ routes }) => {
     return (
         <BrowserRouter>
             <Switch>
-                {routes.map(route => <Route path={route.path} exact={route.exact} render={() => route.render} />)}
+                {routes.map((route, index) =>
+                    <Route
+                        key={index}
+                        path={route.path}
+                        exact={route.exact}
+                        render={() => route.render}
+                    />
+                )}
+
                 <Route component={() => <h1>404 Not Found!</h1>} />
             </Switch>
         </BrowserRouter>
--- src/components/Authentication/Authentication.tsx
+++ src/components/Authentication/Authentication.tsx
@@ -57,7 +57,7 @@ const Authentication: React.FC = () => {
     };
 
     return (
-        <div>
+        <div className="Authentication-container">
             <AppHeader />
             {selectView()}
         </div>
