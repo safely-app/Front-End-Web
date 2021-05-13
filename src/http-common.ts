c50c7895be9e80905ac3c2a8150041ab15d87451
Louis Christner <louis.christner@epitech.eu>
2021-05-13 15:24:10 +0200

    Move to https

--- src/http-common.ts
+++ src/http-common.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const createHttpConfig = (token?: string) => axios.create({
-    baseURL: "http://api.safely-app.fr",
+    baseURL: "https://api.safely-app.fr",
     headers: {
         "Content-type": "application/json",
         Authorization: `Bearer ${token}`
