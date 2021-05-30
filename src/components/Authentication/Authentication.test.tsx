747af7a4321913c27dacdff96370a83678abd58e
Louis Christner <louis.christner@epitech.eu>
2021-05-30 15:06:12 +0200

    Fix Authentication component test

--- src/components/Authentication/Authentication.test.tsx
+++ src/components/Authentication/Authentication.test.tsx
@@ -14,7 +14,7 @@ test('renders authentication sign up component', () => {
     expect(screen.getByText(/Pas encore inscrit/i)).toBeInTheDocument();
     expect(screen.getByRole("email")).toBeInTheDocument();
     expect(screen.getByRole("password")).toBeInTheDocument();
-    expect(screen.getAllByRole("button").length).toEqual(2);
+    expect(screen.getAllByRole("button").length).toEqual(3);
 });
 
 test('renders authentication sign in component', () => {
