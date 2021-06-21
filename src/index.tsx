91126ff69efb0b0099fc46e3f6eb88faadcabbf5
Louis Christner <louis.christner@epitech.eu>
2021-06-21 20:35:28 +0200

    Add missing comma

--- src/index.tsx
+++ src/index.tsx
@@ -23,7 +23,7 @@ const routes = [
     { path: '/logout', exact: true, protected: false, render: <SignOut /> },
     { path: '/admin', exact: true, protected: true, render: <Monitor /> },
     { path: '/safeplace', exact: true, protected: true, render: <Safeplace /> },
-    { path: '/profile', exact: true, protected: true, render: <Profile /> }
+    { path: '/profile', exact: true, protected: true, render: <Profile /> },
     { path: '/reset', exact: false, protected: false, render: <ResetPassword /> }
 ];
 
