c6472e2c238593a43b3aba23f1baba1f750d35fd
Louis Christner <louis.christner@epitech.eu>
2021-07-11 14:50:44 +0200

    Create modal component and update style

--- src/components/common/index.tsx
+++ src/components/common/index.tsx
@@ -2,10 +2,12 @@ import TextInput from './TextInput';
 import Button from './Button';
 import Dropdown from './Dropdown';
 import List from './List';
+import Modal from './Modal';
 
 export {
     TextInput,
     Button,
     Dropdown,
-    List
+    List,
+    Modal
 };
