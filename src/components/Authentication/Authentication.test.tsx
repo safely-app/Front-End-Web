8cee365715f37bd251869339c225b67641449a92
Louis Christner <louis.christner@epitech.eu>
2021-08-06 15:12:01 +0200

    Add Authentication tests

--- src/components/Authentication/Authentication.test.tsx
+++ src/components/Authentication/Authentication.test.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
-import { Authentication } from './Authentication';
+import {
+    Authentication,
+    ResetPassword
+} from './Authentication';
 import { Provider } from 'react-redux';
 import { store } from '../../redux';
 
@@ -29,11 +32,19 @@ test('renders authentication sign in component', () => {
 
     fireEvent.click(switchViewButton);
 
-    expect(screen.getByText(/Déjà inscrit/i)).toBeInTheDocument();
+    const alreadySubscribedButton = screen.getByText(/Déjà inscrit/i);
+    expect(alreadySubscribedButton).toBeInTheDocument();
+
     expect(screen.getByRole("email")).toBeInTheDocument();
     expect(screen.getByRole("username")).toBeInTheDocument();
     expect(screen.getAllByRole("password").length).toEqual(2);
     expect(screen.getAllByRole("button").length).toEqual(2);
+
+    fireEvent.click(alreadySubscribedButton);
+
+    expect(screen.getByRole("email")).toBeInTheDocument();
+    expect(screen.getByRole("password")).toBeInTheDocument();
+    expect(screen.getAllByRole("button").length).toEqual(3);
 });
 
 test('renders authentication forgotten password component', () => {
@@ -50,4 +61,44 @@ test('renders authentication forgotten password component', () => {
 
     expect(screen.getByRole("email")).toBeInTheDocument();
     expect(screen.getByRole("button")).toBeInTheDocument();
+});
+
+test('renders authentication with filled information', () => {
+    render(
+        <Provider store={store}>
+            <Authentication />
+        </Provider>
+    );
+
+    const email = screen.getByRole("email");
+    const password = screen.getByRole("password");
+    const buttons = screen.getAllByRole("button");
+    const loginButton = buttons[0];
+
+    expect(buttons.length).toEqual(3);
+    expect(email).toBeInTheDocument();
+    expect(password).toBeInTheDocument();
+    expect(loginButton).toBeInTheDocument();
+
+    fireEvent.change(email, {
+        target: { value: "email@test.com" }
+    });
+
+    fireEvent.change(password, {
+        target: { value: "passwordtest" }
+    });
+
+    expect(screen.getByDisplayValue("email@test.com")).toBeInTheDocument();
+    expect(screen.getByDisplayValue("passwordtest")).toBeInTheDocument();
+});
+
+test('renders reset password component', async () => {
+    render(
+        <Provider store={store}>
+            <ResetPassword />
+        </Provider>
+    );
+
+    expect(screen.getByRole("button")).toBeInTheDocument();
+    expect(screen.getAllByRole("password").length).toEqual(2);
 });
