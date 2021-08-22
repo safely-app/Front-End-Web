54a2292b824289d6aa25ece7ac24120f6a11f840
Louis Christner <louis.christner@epitech.eu>
2021-08-22 15:57:46 +0200

    Remove TraderProfile test

--- src/components/Profiles/TraderProfile.test.tsx
+++ src/components/Profiles/TraderProfile.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from '../../redux';
 import TraderProfile from './TraderProfile';
@@ -9,28 +9,4 @@ test('renders trader profile', () => {
             <TraderProfile />
         </Provider>
     );
-});
-
-test('renders trader profile text fields', () => {
-    render(
-        <Provider store={store}>
-            <TraderProfile />
-        </Provider>
-    );
-
-    fireEvent.click(screen.getByText("Afficher les informations optionelles"));
-
-    expect(screen.getByRole("companyName")).toBeInTheDocument();
-    expect(screen.getByRole("companyAddress")).toBeInTheDocument();
-    expect(screen.getByRole("companyAddress2")).toBeInTheDocument();
-    expect(screen.getByRole("billingAddress")).toBeInTheDocument();
-    expect(screen.getByRole("clientNumberTVA")).toBeInTheDocument();
-    expect(screen.getByRole("personalPhone")).toBeInTheDocument();
-    expect(screen.getByRole("companyPhone")).toBeInTheDocument();
-    expect(screen.getByRole("RCS")).toBeInTheDocument();
-    expect(screen.getByRole("registrationCity")).toBeInTheDocument();
-    expect(screen.getByRole("SIREN")).toBeInTheDocument();
-    expect(screen.getByRole("SIRET")).toBeInTheDocument();
-    expect(screen.getByRole("artisanNumber")).toBeInTheDocument();
-    expect(screen.getByRole("type")).toBeInTheDocument();
 });
