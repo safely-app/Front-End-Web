523a2180fb1bcc7a0c616e157e0a1324b102c26e
Louis Christner <louis.christner@epitech.eu>
2021-08-27 14:09:35 +0200

    Remove unused import

--- src/components/Monitor/UserMonitor/UserMonitor.test.tsx
+++ src/components/Monitor/UserMonitor/UserMonitor.test.tsx
@@ -3,7 +3,6 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from '../../../redux';
 import UserMonitor from './UserMonitor';
-import nock from 'nock';
 
 it('renders user monitor', () => {
     render(
