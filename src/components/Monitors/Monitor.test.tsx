c34c029e63c48d7cf5ec3da3facd123e689a96a2
Louis Christner <louis.christner@epitech.eu>
2021-08-30 15:53:23 +0200

    Add Monitor navbar test

--- src/components/Monitors/Monitor.test.tsx
+++ src/components/Monitors/Monitor.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from '../../redux';
 import Monitor from './Monitor';
@@ -10,4 +10,26 @@ test('renders monitor', () => {
             <Monitor />
         </Provider>
     );
+});
+
+test('ensure that monitor navbar works fine', () => {
+    render(
+        <Provider store={store}>
+            <Monitor />
+        </Provider>
+    );
+
+    const userButtonId = 'Créer un nouvel utilisateur-button-id';
+    const userButton = screen.getByTestId('Utilisateurs-navbar-button-id');
+    const safeplaceButton = screen.getByTestId('Safeplaces-navbar-button-id');
+    expect(safeplaceButton).toBeInTheDocument();
+    expect(userButton).toBeInTheDocument();
+
+    fireEvent.click(safeplaceButton);
+
+    expect(screen.queryByTestId(userButtonId)).toBeNull();
+
+    fireEvent.click(userButton);
+
+    expect(screen.getByTestId(userButtonId)).toBeInTheDocument();
 });
