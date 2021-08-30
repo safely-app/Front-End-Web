1b17b327ad9b2c2b2dad2730a7e01779c1bd0359
Louis Christner <louis.christner@epitech.eu>
2021-08-30 13:23:19 +0200

    Remove unused import

--- src/components/common/index.test.tsx
+++ src/components/common/index.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { findByLabelText, fireEvent, queryByDisplayValue, queryByRole, render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { Button } from './index';
 import { TextInput } from './index';
 import { Dropdown } from './index';
