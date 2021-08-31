a5213969175460a9e4810c7d2c62f4d8a5d61c8c
Louis Christner <louis.christner@epitech.eu>
2021-09-01 00:43:31 +0200

    Add user filtering test

--- src/components/Monitor/UserMonitor/UserMonitor.test.tsx
+++ src/components/Monitor/UserMonitor/UserMonitor.test.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
-import { fireEvent, render, screen } from '@testing-library/react';
+import { act, fireEvent, render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from '../../../redux';
 import UserMonitor from './UserMonitor';
+import nock from 'nock';
+
+const testDelay = (ms: number): Promise<void> =>
+    new Promise(resolve => setTimeout(resolve, ms));
 
 it('renders user monitor', () => {
     render(
@@ -73,4 +77,53 @@ it('ensure that create new user is working', () => {
     expect(screen.queryByDisplayValue('test')).toBeNull();
     expect(screen.queryByDisplayValue('test@test.com')).toBeNull();
     expect(screen.queryByDisplayValue('user')).toBeNull();
+});
+
+it('ensure that user filtering is working', async () => {
+    const scope = nock('https://api.safely-app.fr')
+        .get('/user')
+        .reply(200, [
+            {
+                _id: "1",
+                username: "Billy",
+                email: "billy@lesinge",
+                password: "billylesinge",
+                role: "admin"
+            },
+            {
+                _id: "2",
+                username: "Benjamin",
+                email: "benjamin@legorille.com",
+                password: "benjaminlegorille",
+                role: "user"
+            }
+        ], {
+            'Access-Control-Allow-Origin': '*'
+        });
+
+    render(
+        <Provider store={store}>
+            <UserMonitor />
+        </Provider>
+    );
+
+    const userTypeDropdown = screen.getByTestId('all-dropdown-id');
+    const userInfoSearchBar = screen.getByRole('search-bar');
+
+    await act(async () => await testDelay(3000));
+    expect(userInfoSearchBar).toBeInTheDocument();
+    expect(userTypeDropdown).toBeInTheDocument();
+
+    fireEvent.change(userTypeDropdown, {
+        target: { value: 'user' }
+    });
+
+    fireEvent.change(userInfoSearchBar, {
+        target: { value: 'benjamin' }
+    });
+
+    expect(screen.getByDisplayValue('benjamin')).toBeInTheDocument();
+    expect(screen.getByDisplayValue('user')).toBeInTheDocument();
+
+    scope.done();
 });
