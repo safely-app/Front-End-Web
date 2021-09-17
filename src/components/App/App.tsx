033434d51fd6ecd2469feeb06bac5d07216bd7ab
Louis Christner <louis.christner@epitech.eu>
2021-09-17 09:29:39 +0200

    Update App component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -69,7 +69,7 @@ const App: React.FC = () => {
         }).catch(error => {
             log.error(error);
         });
-    }, [userCredientials]);
+    }, [userCredientials, dispatch]);
 
     return (
         <div className="App">
--- src/components/Safeplace/Safeplace.tsx
+++ src/components/Safeplace/Safeplace.tsx
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import { RootState } from '../../redux';
 import { Safeplace } from '../../services';
 import { AppHeader } from '../Header/Header';
 import ISafeplace from '../interfaces/ISafeplace';
