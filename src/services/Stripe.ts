4ad280399a5ee9b319073951738b629616239d04
Louis Christner <louis.christner@epitech.eu>
2021-10-01 03:40:36 +0200

    Update Stripe url

--- src/services/Stripe.ts
+++ src/services/Stripe.ts
@@ -5,21 +5,21 @@ class Stripe {
     private readonly baseURL = process.env.REACT_APP_SERVER_URL as string;
 
     get(id: string, token: string) {
-        return createHttpConfig(this.baseURL, token).get(`/Stripe/user/${id}`);
+        return createHttpConfig(this.baseURL, token).get(`/stripe/user/${id}`);
     }
 
     create(data: IStripe, token: string) {
         const { id, ...stripeData } = data;
-        return createHttpConfig(this.baseURL, token).post('/Stripe/user', stripeData);
+        return createHttpConfig(this.baseURL, token).post('/stripe/user', stripeData);
     }
 
     update(_id: string, data: IStripe, token: string) {
         const { id, ...stripeData } = data;
-        return createHttpConfig(this.baseURL, token).put(`/Stripe/user/${_id}`, stripeData);
+        return createHttpConfig(this.baseURL, token).put(`/stripe/user/${_id}`, stripeData);
     }
 
     linkCard(data: { cardId: string, stripeId: string }, token: string) {
-        return createHttpConfig(this.baseURL, token).post('/Stripe/cardLink', data);
+        return createHttpConfig(this.baseURL, token).post('/stripe/cardLink', data);
     }
 }
 
