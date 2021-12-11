b931bfd0385a61860adcd704bbec1adfa7fe5d0f
Louis Christner <louis.christner@epitech.eu>
2021-12-11 07:17:46 +0100

    Fix issues

--- src/components/Monitors/UserMonitor/UserMonitor.tsx
+++ src/components/Monitors/UserMonitor/UserMonitor.tsx
@@ -59,7 +59,7 @@ const UserInfoForm: React.FC<IUserInfoProps> = ({
     return (
         <Modal shown={(shown !== undefined) ? shown : true} content={
             <div className="User-Info">
-                <TextInput key={`${user.id}-id`} type="text" role="username"
+                <TextInput key={`${user.id}-id`} type="text" role="id"
                     label="Identifiant" value={user.id} setValue={() => {}} readonly={true} />
                 <TextInput key={`${user.id}-username`} type="text" role="username"
                     label="Nom d'utilisateur" value={user.username} setValue={setUsername} />
