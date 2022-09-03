bfeb780cfe74d67f8cef9cf3be6a1cbbec64c875
Louis Christner <louis.christner@epitech.eu>
2022-09-03 17:52:37 +0200

    Update BillingMonitor

--- src/components/Monitors/BillingMonitor/BillingMonitor.tsx
+++ src/components/Monitors/BillingMonitor/BillingMonitor.tsx
@@ -1,17 +1,12 @@
 import { useEffect, useState } from "react";
 import { BsPencilSquare } from "react-icons/bs";
-import { ImCross } from "react-icons/im";
 import { useAppSelector } from "../../../redux";
 import { Billing } from "../../../services";
 import { SearchBar, Table } from "../../common";
 import IBilling from "../../interfaces/IBilling";
-import { notifyError } from "../../utils";
+import { convertStringToRegex, notifyError, notifySuccess } from "../../utils";
 import log from "loglevel";
-
-enum ModalType {
-  CREATE,
-  OFF
-}
+import { ModalBtn, ModalType, SafeplaceCreateModal, SafeplaceUpdateModal } from "./BillingMonitorModal";
 
 const CustomDiv: React.FC<{
   content: JSX.Element | string;
@@ -26,12 +21,22 @@ const CustomDiv: React.FC<{
 };
 
 const BillingMonitor: React.FC = () => {
+  const userInfo = useAppSelector(state => state.user.userInfo);
   const userCredentials = useAppSelector(state => state.user.credentials);
 
   const [billings, setBillings] = useState<IBilling[]>([]);
   const [textSearch, setTextSearch] = useState("");
-  const [modalOn, setModalOn] = useState(ModalType.OFF);
-  const [modalTypes, setModalTypes] = useState<ModalType[]>([]);
+  const [modalOn, setModal] = useState(ModalType.OFF);
+
+  const [billing, setBilling] = useState<IBilling>({
+    id: "",
+    amount: 0,
+    currency: "",
+    receiptEmail: "",
+    paymentMethod: "",
+    description: "",
+    status: ""
+  });
 
   const keys = [
     { displayedName: 'MONTANT', displayFunction: (billing: IBilling, index: number) => <CustomDiv key={'tbl-val-' + index} content={String(billing.amount)} /> },
@@ -42,17 +47,60 @@ const BillingMonitor: React.FC = () => {
     { displayedName: 'ID', displayFunction: (billing: IBilling, index: number) => <CustomDiv key={'tbl-val-' + index} content={billing.id} /> },
     { displayedName: 'ACTION', displayFunction: (billing: IBilling, index: number) =>
       <CustomDiv key={'tbl-val-' + index} content={
-        <div className="ml-3 flex space-x-2">
-          <button onClick={() => {}}><BsPencilSquare /></button>
-          <button onClick={() => {}}><ImCross /></button>
+        <div className="ml-3 flex">
+          <button onClick={() => updateModal(billing, ModalType.UPDATE)}><BsPencilSquare /></button>
         </div>
       } />
     },
   ];
 
-  const setModal = (modalType: ModalType) => {
-    setModalTypes([ ...modalTypes, modalType ]);
-    setModalOn(modalType);
+  const filterBillings = (): IBilling[] => {
+    const lowerSearchText = convertStringToRegex(textSearch.toLocaleLowerCase());
+
+    if (textSearch === '') {
+      return billings;
+    }
+
+    return billings
+      .filter(billing => textSearch !== ''
+        ? billing.currency.toLowerCase().match(lowerSearchText) !== null
+        || billing.id.toLowerCase().match(lowerSearchText) !== null
+        || billing.paymentMethod.toLowerCase().match(lowerSearchText) !== null
+        || billing.receiptEmail.toLowerCase().match(lowerSearchText) !== null
+        || billing.status.toLowerCase().match(lowerSearchText) !== null
+        || billing.amount.toString().toLowerCase().match(lowerSearchText) !== null
+        || billing.description.toLowerCase().match(lowerSearchText) !== null : true);
+  };
+
+  const updateModal = (billing: IBilling, modalType: ModalType) => {
+    setModal(modalType);
+    setBilling(billing);
+  };
+
+  const createBilling = async (billing: IBilling) => {
+    try {
+      const response = await Billing.create(billing, userCredentials.token);
+      const newBilling = { ...billing, id: response.data._id };
+
+      setBillings([ ...billings, newBilling ]);
+      notifySuccess("Nouvelle facture créée");
+      setModal(ModalType.OFF);
+    } catch (err) {
+      notifyError(err);
+      log.error(err);
+    }
+  };
+
+  const updateBilling = async (billing: IBilling) => {
+    try {
+      await Billing.update(billing.id, userInfo.stripeId || "", billing, userCredentials.token);
+      setBillings(billings.map(b => (b.id === billing.id) ? billing : b));
+      notifySuccess("Modifications enregistrées");
+      setModal(ModalType.OFF);
+    } catch (err) {
+      notifyError(err);
+      log.error(err);
+    }
   };
 
   useEffect(() => {
@@ -77,6 +125,28 @@ const BillingMonitor: React.FC = () => {
   return (
     <div className='my-3'>
 
+      <SafeplaceCreateModal
+        title="Créer une nouvelle facture"
+        modalOn={modalOn === ModalType.CREATE}
+        billing={billing}
+        setBilling={setBilling}
+        buttons={[
+          <ModalBtn content="Modifier la safeplace" onClick={() => createBilling(billing)} />,
+          <ModalBtn content="Annuler" onClick={() => setModal(ModalType.OFF)} />
+        ]}
+      />
+
+      <SafeplaceUpdateModal
+        title="Modifier une facture"
+        modalOn={modalOn === ModalType.UPDATE}
+        billing={billing}
+        setBilling={setBilling}
+        buttons={[
+          <ModalBtn content="Modifier la safeplace" onClick={() => updateBilling(billing)} />,
+          <ModalBtn content="Annuler" onClick={() => setModal(ModalType.OFF)} />
+        ]}
+      />
+
       <SearchBar
         placeholder='Rechercher une facture...'
         textSearch={textSearch}
@@ -84,7 +154,7 @@ const BillingMonitor: React.FC = () => {
         openCreateModal={() => setModal(ModalType.CREATE)}
       />
       <div className='mt-3'>
-        <Table content={billings} keys={keys} />
+        <Table content={filterBillings()} keys={keys} />
       </div>
     </div>
   );
