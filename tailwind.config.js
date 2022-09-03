64c69111b1289c516baef74044c8312d19f656b9
Louis Christner <louis.christner@epitech.eu>
2022-09-03 17:27:59 +0200

    Fix tailwind config issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  media: false, // or 'media' or 'class'
   theme: {
     extend: {
       backgroundImage: theme => ({
