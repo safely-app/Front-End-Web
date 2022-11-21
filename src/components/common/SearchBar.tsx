50ae33f465c478fe4361747118927dffa827271f
Louis Christner <louis.christner@epitech.eu>
2022-11-21 21:01:19 +0100

    Update Safeplaces SearchBar

--- src/components/common/SearchBar.tsx
+++ src/components/common/SearchBar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import {
   FaPlus,
   FaPlusCircle,
@@ -10,15 +10,22 @@ const SearchBar: React.FC<{
   textSearch: string;
   placeholder: string;
   setTextSearch: (value: string) => void;
+  onInputFocus?: () => void;
+  onInputBlur?: () => void;
+  onKeyDown?: (event: React.KeyboardEvent<HTMLInputElement>, ref: React.MutableRefObject<any>) => void;
   openCreateModal: () => void;
   noCreate?: boolean;
 }> = ({
   textSearch,
   placeholder,
   setTextSearch,
+  onInputFocus,
+  onInputBlur,
+  onKeyDown,
   openCreateModal,
   noCreate
 }) => {
+  const searchBarInput = useRef(null);
   const [isMouseOn, setIsMouseOut] = useState(false);
 
   return (
@@ -26,10 +33,14 @@ const SearchBar: React.FC<{
       <div className='relative'>
         <div className='invisible text-sm h-0 pr-6'>{placeholder}</div>
         <input
+          ref={searchBarInput}
           className='text-sm min-w-52 w-full border-b-2 border-solid border-blue-400 bg-neutral-100'
           placeholder={placeholder}
           value={textSearch}
+          onKeyDown={(event) => onKeyDown !== undefined ? onKeyDown(event, searchBarInput) : {}}
           onChange={(event) => setTextSearch(event.target.value)}
+          onFocus={() => onInputFocus !== undefined ? onInputFocus() : {}}
+          onBlur={() => onInputBlur !== undefined ? onInputBlur() : {}}
         />
         <button className='absolute right-1 top-1'><FaSearch className='text-blue-400' /></button>
       </div>
