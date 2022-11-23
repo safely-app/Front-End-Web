c4e4589645aa9de8e9e3163b1c04c74ac248b0ef
Louis Christner <louis.christner@epitech.eu>
2022-11-23 22:33:46 +0100

    Fix BugReport error
    Add preventDefault to form submit

--- src/components/BugReport/BugReport.tsx
+++ src/components/BugReport/BugReport.tsx
@@ -56,36 +56,36 @@ const BugReport: React.FC = () => {
       <AppHeader />
 
       <section className="bg-white dark:bg-gray-900">
-  <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-      <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contactez-nous</h2>
-      <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Avez-vous un problème technique ? Souhaitez vous nous faire part de vos commentaires sur une fonctionnalité ? Avez-vous besoin de détails sur nos abonnements ? Faites-le nous savoir.</p>
-      <form action="#" className="space-y-8">
-          <div>
+        <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contactez-nous</h2>
+          <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Avez-vous un problème technique ? Souhaitez vous nous faire part de vos commentaires sur une fonctionnalité ? Avez-vous besoin de détails sur nos abonnements ? Faites-le nous savoir.</p>
+          <form action="#" className="space-y-8" onSubmit={(event) => { event.preventDefault(); }}>
+            <div>
               <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">J'aimerais</label>
               <select className="w-full border border-gray-200 p-2 focus:outline-none focus:border-gray-500" value={report.type} onChange={(event) => setField('type', event)}>
-              <option value="Bug">Rapporter un bug</option>
-              <option value="Feature">Requête d'une fonctionalité</option>
-              <option value="Opinion">Commentaire</option>
-            </select>
-          </div>
-          <div>
+                <option value="Bug">Rapporter un bug</option>
+                <option value="Feature">Requête d'une fonctionalité</option>
+                <option value="Opinion">Commentaire</option>
+              </select>
+            </div>
+            <div>
               <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Titre</label>
-              <input  value={report.title} onChange={(event) => setField('title', event)} type="text" id="subject" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Titre de votre requête" required/>
-          </div>
-          <div className="sm:col-span-2">
+              <input value={report.title} onChange={(event) => setField('title', event)} type="text" id="subject" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Titre de votre requête" required />
+            </div>
+            <div className="sm:col-span-2">
               <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Description</label>
-              <textarea  value={report.comment} onChange={(event) => setField('comment', event)} id="message" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Décrivez-nous votre avis"></textarea>
-          </div>
-          <div className="flex justify-center submit">
-          <button className="text-lg font-bold text-white bg-blue-600 hover:bg-blue-500 px-6 py-2 rounded-lg float-right" onClick={sendReport}>
-              Envoyer
-            </button>
-          </div>
-      </form>
-  </div>
-</section>
-      
-      
+              <textarea value={report.comment} onChange={(event) => setField('comment', event)} id="message" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Décrivez-nous votre avis"></textarea>
+            </div>
+            <div className="flex justify-center submit">
+              <button className="text-lg font-bold text-white bg-blue-600 hover:bg-blue-500 px-6 py-2 rounded-lg float-right" onClick={sendReport}>
+                Envoyer
+              </button>
+            </div>
+          </form>
+        </div>
+      </section>
+
+
     </div>
   );
 }
